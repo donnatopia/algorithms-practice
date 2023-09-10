de7577e0a5c3d70e2bb01b1dbc5c3ca692cb61e5
Donna <donnatopia@gmail.com>
2023-09-10 13:06:19 -0700

    added solution to linkedListIntersection

--- toy-problems/26. Linked List Intersection/linkedListIntersection.js
+++ toy-problems/26. Linked List Intersection/linkedListIntersection.js
@@ -18,3 +18,41 @@ const linkedListIntersection = (list1, list2) => {
 }
 
 module.exports = linkedListIntersection;
+
+// // time complexity is O(n)
+// // space complexity is O(1)
+// const linkedListIntersectionSoln = (list1, list2) => {
+//   // determine which is the longer list
+//   const length1 = getLinkedListLength(list1);
+//   const length2 = getLinkedListLength(list2);
+
+//   let diff = Math.abs(length1 - length2);
+//   let long = length1 >= length2 ? list1 : list2;
+//   let short = length1 >= length2 ? list2 : list1;
+
+//   // iterate through the two list while they exist
+//   while (long && short) {
+//     // if the list length is still greater
+//     // move onto the next node of long
+//     if (diff > 0) {
+//       long = long.next;
+//       diff--;
+//       continue;
+//     }
+
+//     // check to see if the list are the same
+//     if (long === short) return long;
+
+//     // else, check the next node
+//     long = long.next;
+//     short = short.next;
+//   }
+
+//   // afterwards, return null
+//   return null;
+// }
+
+// const getLinkedListLength = (list) => {
+//   if (list.next === null) return 1;
+//   return 1 + getLinkedListLength(list.next);
+// }

b89cc0355497f8d51c3f02628a7c0788f3691654
Donna <donnatopia@gmail.com>
2023-09-11 22:32:21 -0700

    initialized pop and size function + added respective tests

--- toy-problems/4. Constant Time Stack Min/constantTimeStackMin.js
+++ toy-problems/4. Constant Time Stack Min/constantTimeStackMin.js
@@ -1,7 +1,6 @@
 /**
  * @constructor
  */
-
 const Stack = function () {
 
 }
@@ -9,7 +8,6 @@ const Stack = function () {
 /**
  * @param {number} val
  */
-
 Stack.prototype.push = function (val) {
 
 }
@@ -17,8 +15,21 @@ Stack.prototype.push = function (val) {
 /**
  * @return {number}
  */
+Stack.prototype.pop = function () {
 
+}
+
+/**
+ * @return {number}
+ */
 Stack.prototype.min = function () {
+
+}
+
+/**
+ * @return {number}
+ */
+Stack.prototype.size = function () {
 
 }
 
--- toy-problems/4. Constant Time Stack Min/constantTimeStackMin.test.js
+++ toy-problems/4. Constant Time Stack Min/constantTimeStackMin.test.js
@@ -6,14 +6,23 @@ xdescribe('4. Constant Time Stack Min', () => {
     var stack = new Stack();
     stack.push(4);
     expect(stack.min()).toBe(4);
+
     stack.push(3);
     expect(stack.min()).toBe(3);
+
     stack.push(2);
     expect(stack.min()).toBe(2);
+
     stack.push(1);
     expect(stack.min()).toBe(1);
-    stack.push(5);
-    expect(stack.min()).toBe(1);
+
+    stack.pop();
+    expect(stack.min()).toBe(2);
+    expect(stack.size()).toBe(3);
+
+    stack.pop();
+    expect(stack.min()).toBe(3);
+    expect(stack.size()).toBe(2);
   });
 
 });
